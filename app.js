c6c9ab5fa1bd44ee7eceb571ab7706b109f927d1
Rosita <Rositamtz.98@gmail.com>
2022-04-10 22:40:18 -0500

    [Modified] cambios para aceptar el post del front

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');// This will be our application entry. We'll setup our server here.
 
 const http = require('http');
+const cors=require("cors");
+
 // Set up the express app
 const app = express();// Log requests to the console.
 
@@ -16,9 +18,20 @@ app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
 
+
+
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);
 const server = http.createServer(app);
 server.listen(port);
+
+const corsOptions ={
+  //origin:"http://localhost:3000", 
+  origin:"*", 
+  credentials:true,            
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
 
 module.exports = app;
